67fe6c771a63aaa0945f8e9c52606a1aa2a2cca5
Daniel200220 <tcaciovdaniel@gmail.com>
2022-12-14 12:49:16 +0200

    main

--- src/main/java/org/ieti/TcaciovDaniel/Main.java
+++ src/main/java/org/ieti/TcaciovDaniel/Main.java
@@ -2,6 +2,6 @@ package org.ieti.TcaciovDaniel;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Hello");
+        System.out.println("Helllllo");
     }
 }
